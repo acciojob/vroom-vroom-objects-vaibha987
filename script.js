0373f73cf66dbfa6f15869eca18beb3b53d268e6
vaibha987 <v2goyal95@gmail.com>
2024-09-22 17:20:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,22 +2,22 @@
 function Car(make, model) {
 	this.make = make;
     this.model = model;
+}
  Car.prototype.getMakeModel = function() {
     return this.make + " " + this.model;
-}
-	
 }
 
 function SportsCar(make, model, topSpeed) {
 	Car.call(this, make, model);
 	this.topSpeed = topSpeed;
+}
 SportsCar.prototype = Object.create(Car.prototype);
 SportsCar.prototype.constructor = SportsCar;
 
 SportsCar.prototype.getTopSpeed = function() {
     return this.topSpeed;
 }
-}
+
 
 // Do not change the code below
 window.Car = Car;
